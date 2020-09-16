91ed5291984083472115edf2eb7100526b4f98e3
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-09-16 11:20:21 -0700

    Added temporary excuses array in data folder for simulating db

---
+++ client/src/data/excuses.js
@@ -0,0 +1,237 @@
+module.exports = [
+  {
+    id: 1,
+    excuse: "I'm having an allergic reaction",
+    severity: 3
+  },
+  {
+    id: 2,
+    excuse: "Dog are my homework",
+    severity: 1
+  },
+  {
+    id: 3,
+    excuse: "My psychic told me I was going to die if I left the house today",
+    severity: 3
+  },
+  {
+    id: 4,
+    excuse: "I have a dentist appointment",
+    severity: 2
+  },
+  {
+    id: 5,
+    excuse: "I have a doctor appointment",
+    severity: 2
+  },
+  {
+    id: 6,
+    excuse: "My (animal) is sick",
+    severity: 2
+  },
+  {
+    id: 7,
+    excuse: "I have diarrhea",
+    severity: 2
+  },
+  {
+    id: 8,
+    excuse: "I have a toothache",
+    severity: 2
+  },
+  {
+    id: 9,
+    excuse: "I'm waiting on a delivery I need to sign for",
+    severity: 2
+  },
+  {
+    id: 10,
+    excuse: "My (someone important)'s graduation is that day",
+    severity: 2
+  },
+  {
+    id: 11,
+    excuse: "My boss scheduled me to work today",
+    severity: 2
+  },
+  {
+    id: 12,
+    excuse: "I could not find a babysitter for my kid",
+    severity: 2
+  },
+  {
+    id: 13,
+    excuse: "I have to babysit for my (anybody)",
+    severity: 2
+  },
+  {
+    id: 14,
+    excuse: "I have a cold",
+    severity: 1
+  },
+  {
+    id: 15,
+    excuse: "I have a headache",
+    severity: 1
+  },
+  {
+    id: 16,
+    excuse: "I'm tired",
+    severity: 1
+  },
+  {
+    id: 17,
+    excuse: "I'm taking a mental health day",
+    severity: 1
+  },
+  {
+    id: 18,
+    excuse: "I have food poisoning",
+    severity: 1
+  },
+  {
+    id: 19,
+    excuse: "I'm hungover",
+    severity: 1
+  },
+  {
+    id: 20,
+    excuse: "I ate a burrito and now I can not move",
+    severity: 1
+  },
+  {
+    id: 21,
+    excuse: "Traffic is at a complete stop",
+    severity: 1
+  },
+  {
+    id: 22,
+    excuse: "There is an accident that shut down multiple lanes",
+    severity: 1
+  },
+  {
+    id: 23,
+    excuse: "I nevr actually liked you so I'm not going to make it",
+    severity: 1
+  },
+  {
+    id: 24,
+    excuse: "I threw out my back getting out of bed",
+    severity: 3
+  },
+  {
+    id: 25,
+    excuse: "I am having an allergic reaction",
+    severity: 3
+  },
+  {
+    id: 26,
+    excuse: "I fell in poison ivy",
+    severity: 3
+  },
+  {
+    id: 27,
+    excuse: "My bathroom flooded",
+    severity: 3
+  },
+  {
+    id: 28,
+    excuse: "I'm afraid to be in vehicles when it rains",
+    severity: 3
+  },
+  {
+    id: 29,
+    excuse: "It's my birthday so I can do what I want",
+    severity: 3
+  },
+  {
+    id: 30,
+    excuse: "My kid is sick so I have to stay home and take care of them",
+    severity: 3
+  },
+  {
+    id: 31,
+    excuse: "My car broke down",
+    severity: 4
+  },
+  {
+    id: 32,
+    excuse: "My (animal) died",
+    severity: 4
+  },
+  {
+    id: 33,
+    excuse: "I'm being evicted",
+    severity: 4
+  },
+  {
+    id: 34,
+    excuse: "I got in a car accident",
+    severity: 4
+  },
+  {
+    id: 35,
+    excuse: "I'm getting a colonoscopy",
+    severity: 4
+  },
+  {
+    id: 36,
+    excuse: "I'm depressed",
+    severity: 4
+  },
+  {
+    id: 37,
+    excuse: "I'm proposing to my significant other",
+    severity: 4
+  },
+  {
+    id: 38,
+    excuse: "I just got mugged",
+    severity: 4
+  },
+  {
+    id: 39,
+    excuse: "I am having surgery",
+    severity: 4
+  },
+  {
+    id: 40,
+    excuse: "My (family member) died",
+    severity: 5
+  },
+  {
+    id: 41,
+    excuse: "I broke my (body part)",
+    severity: 5
+  },
+  {
+    id: 42,
+    excuse: "I'm currently sitting in the back of a police car",
+    severity: 5
+  },
+  {
+    id: 43,
+    excuse: "I am getting a divorce and my significant other is trying to claim all my assets",
+    severity: 5
+  },
+  {
+    id: 44,
+    excuse: "I was bitten by a wild animal at the zoo",
+    severity: 5
+  },
+  {
+    id: 45,
+    excuse: "I'm having a family emergency",
+    severity: 5
+  },
+  {
+    id: 46,
+    excuse: "I need to vote today and can't find my polling place",
+    severity: 5
+  },
+  {
+    id: 47,
+    excuse: "I'm being abducted by aliens",
+    severity: 5
+  },
+]
