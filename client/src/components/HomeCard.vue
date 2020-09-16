6ee13edd8864a1063c763ee6b2ad66b95b1c0676
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-09-16 10:49:40 -0700

    Moved cards from Home page to HomeCard component

---
+++ client/src/components/HomeCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="card">
+      <div class="card-dummy"></div>
+      <button @click="action" class="card-btn">
+        <div class="card-content">
+          <SvgIcon :src="this.iconSrc" width="300px" />
+        </div>
+        <div class="description-mask">
+          <h1>{{ this.title }}</h1>
+          <p>{{ this.description }}</p>
+        </div>
+      </button>
+    </div>
+</template>
+
+<script>
+import SvgIcon from "../components/SvgIcon.vue";
+
+export default {
+  props: ['iconSrc', 'title', 'description', 'action'],
+  components: {
+    SvgIcon
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  position: relative;
+  display: inline-block;
+  border-radius: 5px;
+  background-color: #e2d8ca;
+  box-shadow: 0 0 5px;
+  width: 34%;
+}
+.card:hover {
+  background-color: #e9e2d8;
+}
+.card-btn {
+  padding: 0;
+  border: none;
+}
+.card-dummy {
+  padding-top: 75%;
+}
+.card-content {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.description-mask {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-radius: 5px;
+  background-color: rgba(43, 41, 47, 0);
+  color: rgba(0, 0, 0, 0);
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.description-mask:hover {
+  background-color: rgba(43, 41, 47, 0.87);
+  color: #e2d8ca;
+}
+h1 {
+  font-weight: bold;
+  font-size: 3rem;
+}
+p {
+  font-size: 1.5rem;
+}
+</style>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,38 +1,26 @@
 <template>
   <div class="cards">
-    <div class="card">
-      <div class="card-dummy"></div>
-      <button @click="goToExcuses" class="card-btn">
-        <div class="card-content">
-          <SvgIcon :src="require('@/assets/excuses.svg')" width="300px" />
-        </div>
-        <div class="description-mask">
-          <h1>Go to Excuses</h1>
-          <p>Create a new excuse or use an existing one!</p>
-        </div>
-      </button>
-    </div>
-    <div class="card">
-      <div class="card-dummy"></div>
-      <button @click="goToEvents" class="card-btn">
-        <div class="card-content">
-          <SvgIcon :src="require('@/assets/events.svg')" width="300px" />
-        </div>
-        <div class="description-mask">
-          <h1>Go to Events</h1>
-          <p>Check out your upcoming events and excuses!</p>
-        </div>
-      </button>
-    </div>
+    <HomeCard
+      :iconSrc="require('@/assets/excuses.svg')"
+      title="Go to Excuses"
+      description="Create a new excuse or use an existing one!"
+      :action="goToExcuses"
+    />
+    <HomeCard
+      :iconSrc="require('@/assets/events.svg')"
+      title="Go to Events"
+      description="Check out your upcoming events and excuses!"
+      :action="goToEvents"
+    />
   </div>
 </template>
 
 <script>
-import SvgIcon from "../components/SvgIcon.vue";
+import HomeCard from "../components/HomeCard.vue";
 
 export default {
   components: {
-    SvgIcon
+    HomeCard,
   },
   methods: {
     goToExcuses() {
@@ -53,59 +41,5 @@ export default {
   margin-top: 20px;
   margin-left: 10%;
   max-height: 400px;
-}
-.card {
-  position: relative;
-  display: inline-block;
-  border-radius: 5px;
-  background-color: #e2d8ca;
-  box-shadow: 0 0 5px;
-  width: 34%;
-}
-.card:hover {
-  background-color: #e9e2d8;
-}
-.card-btn {
-  padding: 0;
-  border: none;
-}
-.card-dummy {
-  padding-top: 75%;
-}
-.card-content {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.description-mask {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  border-radius: 5px;
-  background-color: rgba(43, 41, 47, 0);
-  color: rgba(0, 0, 0, 0);
-  z-index: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.description-mask:hover {
-  background-color: rgba(43, 41, 47, 0.87);
-  color: #e2d8ca;
-}
-h1 {
-  font-weight: bold;
-  font-size: 3rem;
-}
-p {
-  font-size: 1.5rem;
 }
 </style>
